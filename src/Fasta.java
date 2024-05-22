9e60ac3825de1d99c825c99fc3ce6192046835e1
xiaoxiaoerbao <Xudaxing@2018>
2024-05-22 09:31:52 +0800

    add maven support

--- src/Fasta.java
+++ src/Fasta.java
@@ -1,5 +1,10 @@
+import org.apache.commons.lang3.StringUtils;
+
 public class Fasta {
     public static void main(String[] args) {
         System.out.println("ok");
+        String aa = "ss\taa";
+        String[] bb =StringUtils.split(aa, "\t");
+        System.out.println();
     }
 }
