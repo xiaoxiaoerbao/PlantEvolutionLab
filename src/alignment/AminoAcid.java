6ec454a0c90f3d39aa78b86868bb0b33d5b641e5
xiaoxiaoerbao <Xudaxing@2018>
2025-01-08 12:00:43 +0800

    change amino acid order to A,R,N,D,C,Q,E,G,H,I,L,K,M,F,P,S,T,W,Y,V

--- src/alignment/AminoAcid.java
+++ src/alignment/AminoAcid.java
@@ -1,27 +1,31 @@
 package alignment;
 
+/**
+ * Amino acid enumeration class
+ * Order by A,R,N,D,C,Q,E,G,H,I,L,K,M,F,P,S,T,W,Y,V
+ */
 public enum AminoAcid {
 
     A("Ala", "Alanine", (byte) 'A'),
-    C( "Cys", "Cysteine", (byte) 'C'),
-    D( "Asp", "Aspartic acid", (byte) 'D'),
-    E( "Glu", "Glutamic acid", (byte) 'E'),
-    F( "Phe", "Phenylalanine", (byte) 'F'),
-    G( "Gly", "Glycine", (byte) 'G'),
-    H( "His", "Histidine", (byte) 'H'),
-    I( "Ile", "Isoleucine", (byte) 'I'),
-    K( "Lys", "Lysine", (byte) 'K'),
-    L( "Leu", "Leucine", (byte) 'L'),
-    M( "Met", "Methionine", (byte) 'M'),
-    N( "Asn", "Asparagine", (byte) 'N'),
-    P( "Pro", "Proline", (byte) 'P'),
-    Q( "Gln", "Glutamine", (byte) 'Q'),
     R("Arg", "Arginine", (byte) 'R'),
-    S( "Ser", "Serine", (byte) 'S'),
-    T( "Thr", "Threonine", (byte) 'T'),
-    V( "Val", "Valine", (byte) 'V'),
-    W( "Trp", "Tryptophan", (byte) 'W'),
-    Y( "Tyr", "Tyrosine", (byte) 'Y');
+    N("Asn", "Asparagine", (byte) 'N'),
+    D("Asp", "Aspartic acid", (byte) 'D'),
+    C("Cys", "Cysteine", (byte) 'C'),
+    Q("Gln", "Glutamine", (byte) 'Q'),
+    E("Glu", "Glutamic acid", (byte) 'E'),
+    G("Gly", "Glycine", (byte) 'G'),
+    H("His", "Histidine", (byte) 'H'),
+    I("Ile", "Isoleucine", (byte) 'I'),
+    L("Leu", "Leucine", (byte) 'L'),
+    K("Lys", "Lysine", (byte) 'K'),
+    M("Met", "Methionine", (byte) 'M'),
+    F("Phe", "Phenylalanine", (byte) 'F'),
+    P("Pro", "Proline", (byte) 'P'),
+    S("Ser", "Serine", (byte) 'S'),
+    T("Thr", "Threonine", (byte) 'T'),
+    W("Trp", "Tryptophan", (byte) 'W'),
+    Y("Tyr", "Tyrosine", (byte) 'Y'),
+    V("Val", "Valine", (byte) 'V');
 
     private final String threeLetter;
     private final String fullName;
