a97ba0555687644d4c58b6219157e27dee6eafa0
xiaoxiaoerbao <Xudaxing@2018>
2025-11-10 10:03:33 +0800

    main function

--- src/Start.java
+++ src/Start.java
@@ -1,19 +1,6 @@
-import fa.Fasta;
-
-import java.util.stream.IntStream;
-
 public class Start {
 
     public static void main(String[] args) {
-//        String fastaFile = "/home/ubuntu/publicData/001_reference/002_rice/hitom/GCA_000321445.1_Osat_hitom_01_genomic.fna.gz";
-//        String newFile = "/home/ubuntu/publicData/001_reference/002_rice/hitom/Osat_hitom_genomic.fna.gz";
-//        Fasta fasta = Fasta.buildFastaFrom(fastaFile);
-//        int[] chrID = IntStream.range(1, 13).toArray();
-//        String[] newHeader = new String[chrID.length];
-//        for (int i = 0; i < chrID.length; i++) {
-//            newHeader[i] = String.valueOf(chrID[i]);
-//        }
-//        fasta.renameHeader(newHeader);
-//        fasta.writeFasta(newFile);
+        System.out.println();
     }
 }
